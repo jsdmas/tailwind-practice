46c8bec8325825b7fe451f13d61d4c8805dbcd22
jsdmas <sdmas5730@gmail.com>
2023-08-10 18:21:44 +0900

    feat : profile page folder

---
+++ pages/profile/bought.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Bought: NextPage = () => {
+  return null;
+};
+
+export default Bought;
---
+++ pages/profile/loved.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Loved: NextPage = () => {
+  return null;
+};
+
+export default Loved;
